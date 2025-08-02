9587f815894663b82ff37f4c7c8b3ba8dedd9e0d
Nguyen Quoc Phong <quocphongg2003@gmail.com>
2025-08-02 13:07:09 +0700

    fixed send mail order confirmation

--- utils/mail.js
+++ utils/mail.js
@@ -40,26 +40,23 @@ module.exports = class Email {
 	}
 
 	// Send the actual email
-	async send(mailType, subject, ...args) {
-		const [passwordResetExpires, order] = args;
+	async send(mailType, subject, data = {}) {
 		let html, text;
 
-		if (mailType === 'welcome') {
-			html = welcomeHtml(this.firstName, this.to);
-			text = welcomeText(this.firstName, this.to);
-		} else if (mailType === 'passwordReset') {
-			const passwordExpireTime = passwordResetExpires.toLocaleTimeString('en-US', {
-				hour: '2-digit',
-				minute: '2-digit',
-			});
-			html = passwordResetHtml(this.firstName, this.url, passwordExpireTime);
-			text = passwordResetText(this.firstName, this.url, passwordExpireTime);
-		} else if (mailType === 'orderPlaced') {
-			html = orderPlacedHtml(this.firstName, this.email, order);
-			text = orderPlacedText(this.firstName, this.email, order);
+		switch (mailType) {
+			case 'welcome':
+				({ html, text } = this.getWelcomeContent());
+				break;
+			case 'passwordReset':
+				({ html, text } = this.getPasswordResetContent(data.passwordResetExpires));
+				break;
+			case 'orderPlaced':
+				({ html, text } = this.getOrderPlacedContent(data.order));
+				break;
+			default:
+				throw new Error(`Unknown mail type: ${mailType}`);
 		}
 
-		// Define the email options
 		const mailOptions = {
 			from: this.from,
 			to: this.to,
@@ -68,23 +65,31 @@ module.exports = class Email {
 			text,
 		};
 
-		// Create a transport and send email
 		await this.newTransport().sendMail(mailOptions);
 	}
 
-	async sendWelcome() {
-		await this.send('welcome', 'Welcome to PurpleBee Fashion! 💜');
+	getWelcomeContent() {
+		return {
+			html: welcomeHtml(this.firstName, this.to),
+			text: welcomeText(this.firstName, this.to),
+		};
 	}
 
-	async sendPasswordReset(passwordResetExpires) {
-		await this.send(
-			'passwordReset',
-			'Reset Your PurpleBee Fashion Password',
-			passwordResetExpires
-		);
+	getPasswordResetContent(passwordResetExpires) {
+		const passwordExpireTime = passwordResetExpires.toLocaleTimeString('en-US', {
+			hour: '2-digit',
+			minute: '2-digit',
+		});
+		return {
+			html: passwordResetHtml(this.firstName, this.url, passwordExpireTime),
+			text: passwordResetText(this.firstName, this.url, passwordExpireTime),
+		};
 	}
 
-	async sendOrderPlaced(order) {
-		await this.send('orderPlaced', 'Your Order Has Been Placed!', order);
+	getOrderPlacedContent(order) {
+		return {
+			html: orderPlacedHtml(this.firstName, this.email, order),
+			text: orderPlacedText(this.firstName, this.email, order),
+		};
 	}
 };
